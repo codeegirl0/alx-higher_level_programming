177e3ca65e5c55399a8eed90ca145d3140da283a
codeegirl0 <asmaelk65@gmail.com>
2023-10-09 14:09:01 -0700

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    """get out of list"""
-    new_list = []
+    """Remove from list"""
     if idx >= 0 and idx < len(my_list):
-        new_list = my_list
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
-    return (new_list)
+        del my_list[idx]
+    return (my_list)
