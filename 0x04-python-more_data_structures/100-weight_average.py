9f13301f8859dd3342f48258b7a72d63f564a193
codeegirl0 <asmaelk65@gmail.com>
2023-10-11 22:59:18 +0000

    update

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num = 0
+    den = 0
+    for tp in my_list:
+        num += tp[0] * tp[1]
+        den += tp[1]
+    return (num / den)
