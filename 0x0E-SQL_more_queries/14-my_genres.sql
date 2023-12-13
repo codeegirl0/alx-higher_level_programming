2d25167a34a02be1577405c090d8d51e39a5271b
codeegirl0 <asmaelk65@gmail.com>
2023-12-13 18:06:46 +0100

    adding files

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- listing dexter genres shows
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
+
+       INNER JOIN `tv_shows` AS t
+       ON t.`id` = s.`show_id`
+       WHERE t.`title` = "Dexter"
+ ORDER BY g.`name`;
