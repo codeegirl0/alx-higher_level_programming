a28668b8a364f80b38a5490bcf97eda9d92d09e1
codeegirl0 <asmaelk65@gmail.com>
2024-01-26 20:21:32 +0100

    add

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""shiw id of header of a url
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers.get("X-Request-Id"))
